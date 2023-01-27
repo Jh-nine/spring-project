242bd016578bbabbcc22538e09bc18aceb3d3326
Nine <38931221+Jh-nine@users.noreply.github.com>
2023-01-27 13:52:13 +0900

    Initial commit

---
+++ src/main/java/hello/springproject/Controller/HelloController.java
@@ -0,0 +1,23 @@
+package hello.springproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+
+    @GetMapping("requesttest")
+    public String requesttest(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "requesttest";
+    }
+
+
+}
---
+++ src/test/java/hello/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
